18f22f7d2c1d2f42abd548ec2d06c0f1bfa3e291
hrisikesh-WG <hrisikeshn@wacadsgroup.com>
2024-06-24 16:20:40 +0530

    app host and cross origin updated

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
 from src.cloud_storage.aws_storage import S3Uploader
 
 # Flask app to use the S3Uploader
 app = Flask(__name__)
 
+@cross_origin
 @app.route("/")
 def test():
     return {"message": "Hello World!"}
 
+@cross_origin
 @app.route('/upload/<company_name>', methods=['POST'])
 def upload(company_name):
     files = request.files.getlist('file')  # Get list of files from the request
@@ -22,4 +26,4 @@ def upload(company_name):
     return {"results": upload_responses}
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port = 5000)
